00e7e64cbeb5c2cdcbd994a5d2a9ba4eb9119505
Ayush Mann <136476855+AyushMann29@users.noreply.github.com>
2024-12-15 15:13:39 +0530

    Add files via upload

---
+++ Portfolio(90s)/internet/script.js
@@ -0,0 +1,3 @@
+function changeToHome(){
+    window.location.href = "../index.html";
+}
---
+++ Portfolio(90s)/script.js
@@ -0,0 +1,11 @@
+function changeToAoe2(){
+    window.location.href = "./aoe2/index.html";
+}
+
+function changeToCar(){
+    window.location.href = "./car/index.html";
+}
+
+function changeToInternet(){
+    window.location.href = "./internet/index.html";
+}
